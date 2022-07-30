129f9776f5dca6476c196a67a8da52c327109db7
James Sinkala <xinnks@gmail.com>
2022-07-30 22:57:51 +0300

    style: format code

--- index.cjs
+++ index.cjs
@@ -1,54 +1,56 @@
 const { $fetch } = require("ohmyfetch")
 
 class mlAuth {
+  constructor({ client, secret }) {
+    const missingKeys = ["client", "secret"].filter(
+      (key) => !Object.keys(arguments[0]).includes(key)
+    )
+    if (!client || !secret)
+      throw new Error(`You need to add app keys. Missing [${missingKeys}]`)
 
-	constructor({client, secret}){
-		const missingKeys = ["client", "secret"].filter((key) => !Object.keys(arguments[0]).includes(key))
-		if(!client || !secret) throw new Error(`You need to add app keys. Missing [${missingKeys}]`)
+    const keys = Buffer.from(`${client}:${secret}`, "utf8")
+    this.config = {
+      baseURL: "https://api.mlauth.ml",
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Basic ${keys.toString("base64")}`,
+      },
+    }
+    this.client = $fetch
+  }
 
-		const keys = Buffer.from(`${client}:${secret}`, "utf8")
-		this.config = {
-			baseURL: "https://api.mlauth.ml",
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				"Authorization": `Basic ${keys.toString("base64")}`,
-			},
-		}
-		this.client = $fetch
-	}
+  /**
+   * @description Makes a login request for app client
+   * @param {String} email
+   */
+  async login(email) {
+    if (!email) throw new Error("Email is missing")
+    try {
+      return this.client(`/ml/login`, {
+        ...this.config,
+        body: { email },
+      })
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
 
-	/**
-	 * @description Makes a login request for app client
-	 * @param {String} email
-	 */
-	async login(email){
-		if(!email) throw new Error("Email is missing")
-		try {
-			return this.client(`/ml/login`, {
-				...this.config,
-				body: { email }
-			})
-		} catch (error) {
-			throw new Error(error)
-		}
-	}
-
-	/**
-	 * @description Verifies a login request token from the magic link
-	 * @param {String} token
-	 */
-	async verify(token){
-		if(!token) throw new Error("Token is missing")
-		try {
-			return this.client(`/ml/verify`, {
-				...this.config,
-				body: { token }
-			})
-		} catch (error) {
-			throw new Error(error)
-		}
-	}
+  /**
+   * @description Verifies a login request token from the magic link
+   * @param {String} token
+   */
+  async verify(token) {
+    if (!token) throw new Error("Token is missing")
+    try {
+      return this.client(`/ml/verify`, {
+        ...this.config,
+        body: { token },
+      })
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
 }
 
 module.exports = mlAuth
--- index.mjs
+++ index.mjs
@@ -1,52 +1,53 @@
-
 import { $fetch } from "ohmyfetch"
 
 class mlAuth {
+  constructor({ client, secret }) {
+    const missingKeys = ["client", "secret"].filter(
+      (key) => !Object.keys(arguments[0]).includes(key)
+    )
+    if (!client || !secret)
+      throw new Error(`You need to add app keys. Missing [${missingKeys}]`)
 
-	constructor({client, secret}){
-		const missingKeys = ["client", "secret"].filter((key) => !Object.keys(arguments[0]).includes(key))
-		if(!client || !secret) throw new Error(`You need to add app keys. Missing [${missingKeys}]`)
+    this.endpoint = "https://api.mlauth.ml"
+    this.client = $fetch.create({
+      baseURL: this.endpoint,
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Basic ${btoa(`${client}:${secret}`)}`,
+      },
+    })
+  }
 
-		this.endpoint = "https://api.mlauth.ml"
-		this.client = $fetch.create({
-			baseURL: this.endpoint,
-			method: 'POST',
-			headers: {
-		    Accept: 'application/json',
-  			"Authorization": `Basic ${btoa(`${client}:${secret}`)}`
-		  }
-		})
-	}
+  /**
+   * @description Makes a login request for app client
+   * @param {String} email
+   */
+  async login(email) {
+    if (!email) throw new Error("Email is missing")
+    try {
+      return this.client(`/ml/login`, {
+        body: { email },
+      })
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
 
-	/**
-	 * @description Makes a login request for app client
-	 * @param {String} email
-	 */
-	async login(email){
-		if(!email) throw new Error("Email is missing")
-		try {
-			return this.client(`/ml/login`, {
-				body: { email }
-			})
-		} catch (error) {
-			throw new Error(error)
-		}
-	}
-
-	/**
-	 * @description Verifies a login request token from the magic link
-	 * @param {String} token
-	 */
-	async verify(token){
-		if(!token) throw new Error("Token is missing")
-		try {
-			return this.client(`/ml/verify`, {
-				body: { token }
-			})
-		} catch (error) {
-			throw new Error(error)
-		}
-	}
+  /**
+   * @description Verifies a login request token from the magic link
+   * @param {String} token
+   */
+  async verify(token) {
+    if (!token) throw new Error("Token is missing")
+    try {
+      return this.client(`/ml/verify`, {
+        body: { token },
+      })
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
 }
 
 export default mlAuth
